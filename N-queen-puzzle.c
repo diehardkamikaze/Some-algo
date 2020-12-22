5dc5710183257cefb74e1e2ce58e095b4b9f750a
diehardkamikaze <diehardkamikaze@gmail.com>
2020-12-22 21:25:59 +0300

    combed, I will finish for N

--- N-queen-puzzle.c
+++ N-queen-puzzle.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int EightQueens(int i, int *columns, int *y_equalX, int *y_equal_minusX)
+int EightQueens(int i, char *columns, char *y_equalX, char *y_equal_minusX)
 {
 	int result;
 	int j;
@@ -12,34 +12,28 @@ int EightQueens(int i, int *columns, int *y_equalX, int *y_equal_minusX)
 		return (1);
 	while(j < 8)
 	{
-		if(columns[j] == 0 && y_equalX[i+j] == 0 && y_equal_minusX[j - i + 8] == 0) 
+		if(columns[j] == 0 && y_equalX[i + j] == 0 && y_equal_minusX[j - i + 7] == 0) 
 		{
 			columns[j] = 1;
-			y_equalX[i+j] = 1;
-			y_equal_minusX[j - i + 8] = 1;
-		//	printf("%d %d %d\n", i, i+j, 8 - j + i);
+			y_equalX[i + j] = 1;
+			y_equal_minusX[j - i + 7] = 1;
 			result = result + EightQueens(i + 1, columns, y_equalX, y_equal_minusX);
 			columns[j] = 0;
-			y_equalX[i+j] = 0;
-			y_equal_minusX[j - i + 8] = 0;
+			y_equalX[i + j] = 0;
+			y_equal_minusX[j - i + 7] = 0;
 		}
 		j++;
 	}
 	return (result);
 }
 
-int main(int argc, char **argv)
+int main(/*int argc, char **argv*/)
 {
-	char c = '0';
-	int col[8] = {0};
-	int yX[15] = {0};
-	int y_X[16] = {0};
+	char col[8] = {0};
+	char yX[15] = {0};
+	char y_X[15] = {0};
 
-	if(argc == 2)
-	{
-		int g = EightQueens(0, col, yX, y_X);
-		printf("%d", g);
-	}
-	write(1, "\n", 1);
+	//if(argc == 2)
+		printf("%d\n", EightQueens(0, col, yX, y_X));
 	return (0);
 }
