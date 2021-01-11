c36336d1a53c52ded67fc87c6def5f088e496e5f
diehardkamikaze <diehardkamikaze@gmail.com>
2021-01-11 11:47:41 +0300

    user move added

--- tic-tac-toe/tic-tac-toe.c
+++ tic-tac-toe/tic-tac-toe.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <string.h>
+
+char	g_player_choice;
 
 void	show_board(char *board)
 {
@@ -8,24 +11,60 @@ void	show_board(char *board)
 	while (i < 3)
 	{
 		printf("%c | %c | %c\n", board[0 + 3 * i], board[1 + 3 * i], board[2 + 3 * i]);
-		printf("____________\n");
+		printf("----------\n");
 		i++;
 	}
 }
 
+void	player_move(char *board)
+{
+	int i;
+	printf("You play for %c\n", g_player_choice);
+	do {
+		printf("Your move: (0 - 8): \n");
+		scanf("%d", &i);
+	}
+	while(board[i] != ' ' && printf("Incorrect! Choose only empty cells!\n"));
+	board[i] = g_player_choice;
+}
+
+void	AI_move(char *board)
+{
+	int i;
+	int ai_variants[8][2];
+
+	i = 0;
+
+	while (i < 8)
+	{
+		if (i != ' ')
+		{
+			ai_variants[i][0] = 0;
+			ai_variants[i][1] = 0;
+			continue;
+		}
+		
+		i++;
+	}
+}
+
+void	check_result(char *board)
+{
+
+}
+
+//play_the_game!
+
 int		main(void)
 {
-	char player_choice;
+	char board[9];
 
-	player_choice = 0;
-	do
-	{
+	g_player_choice = 0;
+	do {
 		printf("Select X or O\n");
-		scanf(" %c", &player_choice); //whitespace specificator cool
+		scanf(" %c", &g_player_choice); //whitespace specificator cool
 	}
-	while (player_choice != 'X' && player_choice != 'O');
-
-	char board[9] = { 'X', 'O', 'X', ' ', ' ', 'X', ' ', 'O', 'X' };
-	show_board(board);
+	while (g_player_choice != 'X' && g_player_choice != 'O');
+	memset(board, ' ', 9);
 	return (0);
 }
