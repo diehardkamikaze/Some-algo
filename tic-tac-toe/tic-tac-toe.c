196a34b1924fe8ea29162f38c121c9d5bdd6104c
diehardkamikaze <diehardkamikaze@gmail.com>
2021-01-11 06:56:45 +0300

    show_board and test

--- tic-tac-toe/tic-tac-toe.c
+++ tic-tac-toe/tic-tac-toe.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
 
-int main(void)
+void	show_board(char *board)
 {
-	char player_choice = 0;
+	int i;
 
+	i = 0;
+	while (i < 3)
+	{
+		printf("%c | %c | %c\n", board[0 + 3 * i], board[1 + 3 * i], board[2 + 3 * i]);
+		printf("____________\n");
+		i++;
+	}
+}
+
+int		main(void)
+{
+	char player_choice;
+
+	player_choice = 0;
 	do
 	{
 		printf("Select X or O\n");
-		scanf(" %c", &player_choice);
+		scanf(" %c", &player_choice); //whitespace specificator cool
 	}
 	while (player_choice != 'X' && player_choice != 'O');
 
-	printf("%c", player_choice);
+	char board[9] = { 'X', 'O', 'X', ' ', ' ', 'X', ' ', 'O', 'X' };
+	show_board(board);
 	return (0);
 }
